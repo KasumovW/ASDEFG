fd98adfd7652e9bbacef45a83eeb250b8dd08e9d
Kasumov <lord.kasumov@mail.ru>
2024-06-13 23:14:33 +0300

    first commit

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,11 @@
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <a href='/auth'>
+        <button>Перейти на страницу авторизации</button>
+      </a>
+    </div>
+  );
 };
 
 export default Main;
